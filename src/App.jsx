5dc99f85c7575072c92b84a331a4df15740c633d
Zaky Alharits <zakyalharits03@gmail.com>
2025-05-23 23:09:04 +0700

    memperbarui app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./css/style (1).css";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "#fff",
-      }}
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red" width="150px" height="150px" style={{ animation: "pulse 1.5s infinite" }}>
-        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-      </svg>
-      <style>{`
-        @keyframes pulse {
-          0% { transform: scale(1); }
-          50% { transform: scale(1.2); }
-          100% { transform: scale(1); }
-        }
-      `}</style>
-    </div>
-  );
+  useEffect(() => {
+    import("./js/gsap.min.js");
+    import("./js/MeshSurfaceSampler.js");
+    import("./js/OBJLoader.js");
+    import("./js/script (1).js");
+    import("./js/simplex-noise.js");
+    import("./js/three.min.js");
+    import("./js/TrackballControls.js");
+  }, []);
+
+  return <div>{/* Konten aplikasi */}</div>;
 }
 
 export default App;
